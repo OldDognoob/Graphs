0a724272fc658279a1062cf64f213641e54476ab
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-07-18 22:37:42 +0300

    adventure project wip

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -30,6 +31,83 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# NEED TO KNOW:
+    # player.current_room.id --> gives us the position of the player is on
+    # player.current_room.get_exits() --> returns a list of directions or edges traveling from the position we are on.
+    # player.travel() --> moves us the next room
+    # player.travel(get_room_in_direction) --> gives us in which direction we want to move in the room we are.
+    
+    
+
+# A dictionary to store our rooms
+visited = dict()
+
+# direction for reverse navigation
+reverse_nav = {"n":"s","e":"w","s":"n","w":"e"}
+
+# Initialize as Stack
+stack = Stack()
+
+# we add in the stack our player.current_room were player is on
+stack.push(player.current_room)
+
+# Fire in the hole!
+# while looping the length of the rooms we visited less than the length of our 500 maze
+while len(visited) < len(world.rooms):
+    # if the room we are looking for doesn't exist in our dict
+    if player.current_room.id not in visited:
+        # add to dictionary, with values(directions)
+        visited[player.current_room.id] = player.current_room.get_exits()
+        # for every connected room that hasn't been visited, then visit !
+        if len(visited[player.current_room.id]) is not 0:
+            # visit the last room
+            next_room = visited[player.current_room.id].pop()
+            print("pop", next_room)
+            # save the reverse_nav!!
+            stack.push(next_room)
+            # Then append the direction to the traversal path
+            traversal_path.append(next_room)
+            # travel towards to the next room 
+            player.travel(next_room)
+        # if you find a deadend or there any more rooms to explore
+        else:
+            # get the last directions from the previous room 
+            # were you stored them in the stack
+            directions = stack.pop()
+            # get the opposites ones
+            prev_room = reverse_nav[directions]
+            # move to the previous room
+            player.travel(prev_room)
+            # and add it to the traversal_path
+            traversal_path.append(prev_room)
+        # if the room has already been visited repeat the movement!
+    else:
+        if len(visited[player.current_room.id]) is not 0:
+            # visit the last room
+            next_room = visited[player.current_room.id].pop()
+            print("pop",next_room)
+            # save the reverse_nav!
+            stack.push(next_room)
+            #add the direction to the path
+            traversal_path.append(next_room)
+            # move the the next room!
+            player.travel(next_room)
+        # If you find a deadend or there are any other rooms to be explored
+        else:
+        # get the last directions from the previous room 
+            # were you stored them in the stack
+            directions = stack.pop()
+            # get the opposites ones
+            prev_room = reverse_nav[directions]
+            # move to the previous room
+            player.travel(prev_room)
+            # and add it to the traversal_path
+            traversal_path.append(prev_room)
+            
+print("TP:",traversal_path)
+print("VISITED",visited)
+
+
 
 
 # TRAVERSAL TEST
@@ -42,11 +120,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(
-        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
 
 
 #######
